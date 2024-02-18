741e671cc06ea49c8e0ce8a1afa7075feaa7b92a
Gliverson Almeida <gliversonfrp@gmail.com>
2024-02-18 16:31:07 -0300

    refatorando

---
+++ hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+	export LANG=pt_BR
+
+Execução
+
+	puython3 hello.py
+	ou
+	./hello.py
+"""
+__version__ = "0.1.2"
+__author__ = "Gliverson Almeida"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
+
+print(msg[current_language])
+
+
+
+
